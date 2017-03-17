7b183c60ac8b48dfffb3869a7abe3f1c3f5ef8a7
nathking <nathking@uk.ibm.com>
2017-03-17 22:31:42 +0000

    mochatest

---
+++ test/test-hello.js
@@ -0,0 +1,24 @@
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var should = chai.should();
+
+chai.use(chaiHttp);
+
+describe('hello', function() {
+  it('should return 200 on port 8080', function(done) {
+    chai.request('http://localhost:8080')
+      .get('/hello')
+      .end(function(err, res) {
+         res.should.have.status(200);
+         done();
+      });
+  });
+  it('should fail on port 8081', function(done) {
+    chai.request('http://localhost:8081')
+      .get('/hello')
+      .end(function(err, res) {
+         res.should.not.have.status(200);
+         done();
+      });
+  });
+});
