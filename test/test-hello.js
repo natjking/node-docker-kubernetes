4503e3f59b934f506403bfe0a8b9ce66d61641a7
nathking <nathking@uk.ibm.com>
2017-03-22 22:24:20 +0000

    la

--- test/test-hello.js
+++ test/test-hello.js
@@ -5,18 +5,19 @@ var should = chai.should();
 chai.use(chaiHttp);
 
 describe('hello', function() {
-  it('should return 200 on port 8080', function(done) {
+  it('should return ECONNREFUSED  on port 8080', function(done) {
     chai.request('http://localhost:8080')
       .get('/hello')
       .end(function(err, res) {
          if(err) {
-           console.log(err);
+           err.should.have.code('ECONNREFUSED')
+         } else {
+           throw new Error("fail");        
          }
-         res.should.not.have.status(200);
          done();
       });
   });
-  it('should fail on port 8081', function(done) {
+  it('should return 200 on port 8081', function(done) {
     chai.request('http://localhost:8081')
       .get('/hello')
       .end(function(err, res) {
